169beafafd25567a048ccb5e7f40a48c7096b357
Sundaresan Gunasekaran <sundaresan.gunasekaran@syncfusion.com>
2023-07-19 19:55:02 +0530

    feature(REPORT-320537): Bold-Reports 5.2 Release Changes

--- Models/SqlQuery.cs
+++ Models/SqlQuery.cs
@@ -14,7 +14,7 @@ namespace ReportServices.Models
         {
             if (HttpContext.Current.Cache.Get("SalesOrderDetail") == null)
             {
-                using (SqlConnection connection = new SqlConnection("Data Source=dataplatformdemodata.syncfusion.com;Initial Catalog=AdventureWorks2016;user id=demoreadonly@data-platform-demo;password=N@c)=Y8s*1&dh;"))
+                using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.AppSettings["ConnectionQuery"] ))
                 {
                     connection.Open();
 
@@ -29,6 +29,43 @@ namespace ReportServices.Models
                 }
             }
             return null;
+        }
+          public static string getProductCategory()
+        {
+
+            using (SqlConnection connection = new SqlConnection("Data Source=dataplatformdemodata.syncfusion.com;Initial Catalog=AdventureWorks2016;user id=demoreadonly@data-platform-demo;password=N@c)=Y8s*1&dh;"))
+            {
+                connection.Open();
+
+                string queryString = "SELECT DISTINCT ProductCategoryID, Name FROM Production.ProductCategory";
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+
+                using (DataSet ProductCategories = new DataSet())
+                {
+                    adapter.Fill(ProductCategories, "Orders");
+                    HttpContext.Current.Cache.Insert("ProductCategoryDetail", ProductCategories.Tables[0]);
+                    connection.Close();
+                    return JsonConvert.SerializeObject(HttpContext.Current.Cache.Get("ProductCategoryDetail"));
+                }
+            }
+        }
+
+        public static string getProductSubCategory()
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=dataplatformdemodata.syncfusion.com;Initial Catalog=AdventureWorks2016;user id=demoreadonly@data-platform-demo;password=N@c)=Y8s*1&dh;"))
+            {
+                connection.Open();
+                string queryString = $"SELECT ProductSubcategoryID, ProductCategoryID, Name FROM Production.ProductSubcategory ";
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+
+                using (DataSet ProductSubCategories = new DataSet())
+                {
+                    adapter.Fill(ProductSubCategories);
+                    HttpContext.Current.Cache.Insert("ProductSubCategoryDetail", ProductSubCategories.Tables[0]);
+                    connection.Close();
+                    return JsonConvert.SerializeObject(HttpContext.Current.Cache.Get("ProductSubCategoryDetail"));
+                }
+            }
         }
     }
 }
